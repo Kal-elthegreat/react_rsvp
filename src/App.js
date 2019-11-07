60f85bad5234d766cdb25bf13adf11315575b1fb
Kiel Lewis <kiel.lewis@mailchimp.com>
2019-11-06 20:24:44 -0500

    Built more handler functions and added some conditionals

--- src/App.js
+++ src/App.js
@@ -3,42 +3,67 @@ import GuestList from './components/GuestList';
 
 
 class App extends React.Component {
-
-  state= {
+  state = {
+    isFiltered: false,
     guests: [
       {
-        name: 'Tanjiro',
+        name: "Tanjiro",
         isConfirmed: false,
+        isEditing: false
       },
       {
-        name: 'Luffy',
+        name: "Luffy",
         isConfirmed: true,
+        isEditing: false
       },
       {
-        name: 'Deku',
+        name: "Deku",
         isConfirmed: true,
+        isEditing: false
       }
-    ],
-  }
+    ]
+  };
 
-  toggleConfirmationAt = indexToChange => 
+  toggleGuestPropertyAt = (property, indexToChange) =>
     this.setState({
-      guests: this.state.guests.map((guest,index) => {
+      guests: this.state.guests.map((guest, index) => {
         if (index === indexToChange) {
           return {
             ...guest,
-            isConfirmed: !guest.isConfirmed
+            [property]: !guest[property]
           };
         }
         return guest;
       })
     });
 
+  toggleConfirmationAt = index =>
+    this.toggleGuestPropertyAt("isConfirmed", index);
+
+  toggleEditingAt = index => this.toggleGuestPropertyAt("isEditing", index);
+
+  setNameAt = (name, indexToChange) =>
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            name
+          };
+        }
+        return guest;
+      })
+    });
+
+    toggleFilter = () => 
+      this.setState({ isFiltered: !this.state.isFiltered })
+    ;
+
   getTotalInvited = () => this.state.guests.length;
   //getAttendingGuests = () => Arr.filter
-  //getUnconfirmedGuests = () => 
+  //getUnconfirmedGuests = () =>
 
-  render (){
+  render() {
     return (
       <div className="App">
         <header>
@@ -46,36 +71,45 @@ class App extends React.Component {
           <p>A Treehouse App</p>
           <form>
             <input type="text" value="Safia" placeholder="Invite Someone" />
-              <button type="submit" name="submit" value="submit">Submit</button>
+            <button type="submit" name="submit" value="submit">
+              Submit
+            </button>
           </form>
         </header>
-          <div className="main">
-            <div>
-              <h2>Invitees</h2>
-              <label>
-                <input type="checkbox" /> Hide those who haven't responded
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input 
+                type="checkbox" 
+                onChange={this.toggleFilter}
+                checked={this.state.isFiltered}
+                /> Hide those who haven't responded
             </label>
           </div>
-              <table className="counter">
-                <tbody>
-                  <tr>
-                    <td>Attending:</td>
-                    <td>2</td>
-                  </tr>
-                  <tr>
-                    <td>Unconfirmed:</td>
-                    <td>1</td>
-                  </tr>
-                  <tr>
-                    <td>Total:</td>
-                    <td>3</td>
-                  </tr>
-                </tbody>
-              </table>
-              <GuestList 
-              guests={this.state.guests}
-              toggleConfirmationAt={this.toggleConfirmationAt}  
-              />
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList
+            guests={this.state.guests}
+            toggleConfirmationAt={this.toggleConfirmationAt}
+            toggleEditingAt={this.toggleEditingAt}
+            setNameAt={this.setNameAt}
+            isFiltered={this.state.isFiltered}
+          />
         </div>
       </div>
     );
--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Guests from "./Guests";
+import Guest from "./Guest";
 
 const GuestList = props => 
     <ul>
-        {props.guests.map((guest,index) =>
-            <Guests key={index}
+        {props.guests
+        .map((guest,index) =>
+            <Guest key={index}
             name={guest.name}
             isConfirmed={guest.isConfirmed}
+            isEditing={guest.isEditing}
             handleConfirmation={() => props.toggleConfirmationAt(index)}
+            handleToggleEditing={()=> props.toggleEditingAt(index)}
+            setName={text => props.setNameAt(text, index)}
+            isFiltered={props.isFiltered}
             />
         )}
     </ul>
 
 GuestList.propTypes ={
     guests: PropTypes.array.isRequired,
-    toggleConfirmationAt: PropTypes.func.isRequired
+    toggleConfirmationAt: PropTypes.func.isRequired,
+    toggleEditingAt: PropTypes.func.isRequired,
+    setNameAt: PropTypes.func.isRequired,
+    isFiltered: PropTypes.bool.isRequired,
 }
 
 export default GuestList;
